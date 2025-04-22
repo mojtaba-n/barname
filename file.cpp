417e33be9c954836c35ae0aa1b55c47c70b9b4a4
mojtaba <mojtaba.n1385@gmail.com>
2025-04-22 14:30:29 +0330

    setters ana constructor

--- file.cpp
+++ file.cpp
@@ -12,4 +12,19 @@ float balance;
 bool is_active;
 
 public:
+student(int uid,string sid,string n,string mail,float ba,bool active){
+    setuser_id(uid);
+    setstudent_id(sid);
+    setname(n);
+    setemail(mail);
+    setbalance(ba);
+    setis_active(active);
+};
+void setuser_id(int uid);
+void setstudent_id(string sid);
+void setname(string n);
+void setemail(string mail);
+void setbalance(float ba);
+void setis_active(bool active);
+
 };
