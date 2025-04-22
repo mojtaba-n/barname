203c9b186449d30f9252f6e9716607202ff65cef
mojtaba <mojtaba.n1385@gmail.com>
2025-04-22 15:50:56 +0330

    tarif setters be joz email

--- file.cpp
+++ file.cpp
@@ -20,11 +20,26 @@ student(int uid,string sid,string n,string mail,float ba,bool active){
     setbalance(ba);
     setis_active(active);
 };
-void setuser_id(int uid);
-void setstudent_id(string sid);
-void setname(string n);
-void setemail(string mail);
-void setbalance(float ba);
-void setis_active(bool active);
+void setuser_id(int);
+void setstudent_id(string);
+void setname(string);
+void setemail(string);
+void setbalance(float);
+void setis_active(bool);
 
 };
+void student::setuser_id(int uid){
+    if(uid<1000 || uid>9999){
+        user_id=1000;}
+    else user_id=uid ;}
+void student::setstudent_id(string sid){
+    student_id=sid;}
+void student::setname(string n){
+    name=n;}
+void student::setbalance(float ba){
+    if(ba>20||ba<0)
+    balance=11.11;
+    else balance=ba;}
+void student::setis_active(bool active){
+is_active=active;}            
+
