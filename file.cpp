216efcdc2ef2a4f7310ffafc4dc416a142e46d63
mojtaba <mojtaba.n1385@gmail.com>
2025-04-22 13:07:08 +0330

    tarif private

---
+++ file.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class student
+{
+private:
+int user_id;
+string student_id;
+string name;
+string email;
+float balance;
+bool is_active;
+
+public:
+};
